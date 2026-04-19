f9518a2ed9ea9444d94496b9e8c5dc300613ab90
Dadabhau <dadasthete987@gmail.com>
2026-04-19 14:30:47 +0530

    Project setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layouts/header/header';
+import { UserInput } from './features/user-input/user-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, UserInput],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('investment-calculator-app');
